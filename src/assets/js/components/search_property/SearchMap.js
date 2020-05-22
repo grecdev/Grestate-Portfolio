77993f81500a4d12a84a2f4eb55d199f2fbab39a
Grecu Alexandru <grecdev1@gmail.com>
2020-05-22 19:17:07 +0300

    Environment variables added

--- src/assets/js/components/search_property/SearchMap.js
+++ src/assets/js/components/search_property/SearchMap.js
@@ -1,137 +1,123 @@
 import React, {
+  useState,
+  useReducer,
+  useContext,
+  useEffect,
+  memo,
+} from "react";
 
-	useState,
-	useReducer,
-	useContext,
-	useEffect,
-	memo
+import { FetchContext } from "@context/FetchContext";
+import { GlobalContext } from "@context/GlobalContext";
 
-} from 'react';
-
-import { FetchContext } from '@context/FetchContext';
-import { GlobalContext } from '@context/GlobalContext';
-
-import MapReducer from '@reducers/MapReducer';
+import MapReducer from "@reducers/MapReducer";
 import {
+  SET_PROPERTIES,
+  TOGGLE_POPUP,
+  SET_POPUP_INFO,
+} from "@constants/actionTypes";
 
-	SET_PROPERTIES,
-	TOGGLE_POPUP,
-	SET_POPUP_INFO
+import ReactMapGL, { GeolocateControl } from "react-map-gl";
 
-} from '@constants/actionTypes';
-
-import ReactMapGL, { GeolocateControl } from 'react-map-gl';
-
-import Markers from './map/Markers';
-import PopUp from './map/PopUp';
+import Markers from "./map/Markers";
+import PopUp from "./map/PopUp";
 
 const SearchMap = () => {
+  const { location } = useContext(GlobalContext);
 
-	const { location } = useContext(GlobalContext);
+  const { buy_properties, rent_properties } = useContext(FetchContext);
 
-	const {
+  const defaultData = [
+    {
+      coordinates: {
+        latitude: 37.422028,
+        longitude: -122.084052,
+      },
+      country: "United States",
+      city: "California",
+    },
+  ];
 
-		buy_properties,
-		rent_properties
+  const defaultMapState = {
+    properties: defaultData,
+    show_popup: false,
+    popup_info: defaultData,
+  };
 
-	} = useContext(FetchContext);
+  const [map_state, dispatch] = useReducer(MapReducer, defaultMapState);
 
-	const defaultData = [
-		{
-			coordinates: {
-				latitude: 37.422028,
-				longitude: -122.084052
-			},
-			country: 'United States',
-			city: 'California'
-		}
-	];
+  const togglePopup = (val) => dispatch({ type: TOGGLE_POPUP, payload: val });
 
-	const defaultMapState = {
-		properties: defaultData,
-		show_popup: false,
-		popup_info: defaultData
-	}
+  const getPropertyInfo = (index) =>
+    dispatch({ type: SET_POPUP_INFO, payload: map_state.properties[index] });
 
-	const [map_state, dispatch] = useReducer(MapReducer, defaultMapState);
+  const [arr, setArr] = useState([]);
 
-	const togglePopup = val => dispatch({ type: TOGGLE_POPUP, payload: val });
+  useEffect(() => {
+    location.includes("buy-properties") && setArr(buy_properties);
+    location.includes("rental-listings") && setArr(rent_properties);
 
-	const getPropertyInfo = index => dispatch({ type: SET_POPUP_INFO, payload: map_state.properties[index] });
+    const properties = arr.map((item) => {
+      return {
+        coordinates: item.coordinates,
+        country: item.addressLocation,
+        city: item.addressCity,
+        property_id: item.id,
+      };
+    });
 
-	const [arr, setArr] = useState([]);
+    if (arr.length > 0) dispatch({ type: SET_PROPERTIES, payload: properties });
+    else dispatch({ type: SET_PROPERTIES, payload: defaultData });
+  }, [buy_properties, rent_properties, arr]);
 
-	useEffect(() => {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    zoom: 16,
+    latitude: undefined,
+    longitude: undefined,
+  });
 
-		location.includes('buy-properties') && setArr(buy_properties);
-		location.includes('rental-listings') && setArr(rent_properties);
+  useEffect(() => {
+    setViewport((prevState) => ({
+      ...prevState,
+      latitude: map_state.properties[0].coordinates.latitude,
+      longitude: map_state.properties[0].coordinates.longitude,
+    }));
+  }, [map_state.properties]);
 
-		const properties = arr.map(item => {
-
-			return {
-				coordinates: item.coordinates,
-				country: item.addressLocation,
-				city: item.addressCity,
-				property_id: item.id
-			}
-		});
-
-		if(arr.length > 0) dispatch({ type: SET_PROPERTIES, payload: properties });
-		else dispatch({ type: SET_PROPERTIES, payload: defaultData });
-
-	}, [buy_properties, rent_properties, arr]);
-
-	const [viewport, setViewport] = useState({
-		width: '100%',
-		height: '100%',
-		zoom: 16,
-		latitude: undefined,
-		longitude: undefined
-	})
-
-	useEffect(() => {
-
-		setViewport(prevState => ({
-			...prevState, 
-			latitude: map_state.properties[0].coordinates.latitude,
-			longitude: map_state.properties[0].coordinates.longitude
-		}));
-
-	}, [map_state.properties]);
-	
-	return (
-		<section id='search-map'>
-			<ReactMapGL
-				{...viewport}
-				onViewportChange={setViewport}
-				mapboxApiAccessToken='pk.eyJ1IjoiZ3JlY2RldiIsImEiOiJjazc0eTBjZnEwcW9oM2tudncyaGdhemxvIn0.zgIjs4IpmpFNgxJaeVWjzw'
-				mapStyle='mapbox://styles/mapbox/dark-v10'
-			>
-				<Markers
-					data={map_state.properties}
-					togglePopup={togglePopup}
-					getPropertyInfo={getPropertyInfo}
-					showPopup={map_state.show_popup}
-				/>
-
-				<PopUp
-					showPopup={map_state.show_popup}
-					togglePopup={togglePopup}
-					popupInfo={map_state.popup_info}
-				/>
-
-				<GeolocateControl
-          positionOptions={{enableHighAccuracy: true}}
-					trackUserLocation={true}
-					style={{
-						position: 'absolute',
-						top: 20,
-						left: 20
-					}}
+  return (
+    <section id="search-map">
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={process.env.REACT_MAP_KEY}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+      >
+        <Markers
+          data={map_state.properties}
+          togglePopup={togglePopup}
+          getPropertyInfo={getPropertyInfo}
+          showPopup={map_state.show_popup}
         />
-			</ReactMapGL>
-		</section>
-	)
-}
+
+        <PopUp
+          showPopup={map_state.show_popup}
+          togglePopup={togglePopup}
+          popupInfo={map_state.popup_info}
+        />
+
+        <GeolocateControl
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+          style={{
+            position: "absolute",
+            top: 20,
+            left: 20,
+          }}
+        />
+      </ReactMapGL>
+    </section>
+  );
+};
 
 export default memo(SearchMap);
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /* 
 	For development:
@@ -11,28 +11,28 @@
 	2. CSS > CSS.min and link in html file
 */
 
-import '../css/bootstrap.min.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import '../css/style.scss';
+import "../css/bootstrap.min.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "../css/style.scss";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter as Router } from "react-router-dom";
 
-import GlobalContextProvider from '@context/GlobalContext';
-import FetchContextProvider from '@context/FetchContext';
-import AuthenticationContextProvider from '@context/AuthenticationContext';
+import GlobalContextProvider from "@context/GlobalContext";
+import FetchContextProvider from "@context/FetchContext";
+import AuthenticationContextProvider from "@context/AuthenticationContext";
 
 ReactDOM.render(
-	<Router>
-			<GlobalContextProvider>
-				<FetchContextProvider>
-					<AuthenticationContextProvider>
-						<App />
-					</AuthenticationContextProvider>
-				</FetchContextProvider>
-			</GlobalContextProvider>
-	</Router>
-	, document.getElementById('root')
+  <Router>
+    <GlobalContextProvider>
+      <FetchContextProvider>
+        <AuthenticationContextProvider>
+          <App />
+        </AuthenticationContextProvider>
+      </FetchContextProvider>
+    </GlobalContextProvider>
+  </Router>,
+  document.getElementById("root")
 );
