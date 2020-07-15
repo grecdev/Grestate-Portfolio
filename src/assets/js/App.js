6f00b8943b0f1b1a89f0fbc748941a3cf3bf8f6f
Grecu <grecdev1@gmail.com>
2020-07-15 13:59:48 +0300

    weekly deals width bug resolved

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
-import React, { lazy, Suspense, useContext } from "react";
+import React, { lazy, Suspense, useContext } from 'react';
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
 
-import { AuthenticationContext } from "@context/AuthenticationContext";
+import { AuthenticationContext } from '@context/AuthenticationContext';
 
-import Header from "@components/global_layout/header";
-import IntroLoader from "@components/global_layout/IntroLoader";
-import Footer from "@components/global_layout/Footer";
-import ResetScroll from "@components/global_layout/ResetScroll";
+import Header from '@components/global_layout/header';
+import IntroLoader from '@components/global_layout/IntroLoader';
+import Footer from '@components/global_layout/Footer';
+import ResetScroll from '@components/global_layout/ResetScroll';
 
-const HomePage = lazy(() => import("./pages/HomePage"));
-const SearchPropertyPage = lazy(() => import("./pages/SearchPropertyPage"));
+const HomePage = lazy(() => import('./pages/HomePage'));
+const SearchPropertyPage = lazy(() => import('./pages/SearchPropertyPage'));
 const MortageCalculatorPage = lazy(() =>
-  import("./pages/MortageCalculatorPage")
+  import('./pages/MortageCalculatorPage'),
 );
-const AboutPage = lazy(() => import("./pages/AboutPage"));
-const ContactPage = lazy(() => import("./pages/ContactPage"));
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
-const PropertyPage = lazy(() => import("./pages/PropertyPage"));
-const MyAccountPage = lazy(() => import("./pages/MyAccountPage"));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const ContactPage = lazy(() => import('./pages/ContactPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const PropertyPage = lazy(() => import('./pages/PropertyPage'));
+const MyAccountPage = lazy(() => import('./pages/MyAccountPage'));
 
-import AuthenticationModal from "@components/authentication/AuthenticationModal";
+import AuthenticationModal from '@components/authentication/AuthenticationModal';
 
 const App = () => {
   const { login_enabled, signup_enabled, reset_password_enabled } = useContext(
-    AuthenticationContext
+    AuthenticationContext,
   );
 
   const fallback_style = {
-    fontSize: "2rem",
-    fontWeight: "bold",
-    padding: "3rem",
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    padding: '3rem',
   };
 
   return (
@@ -47,40 +47,40 @@ const App = () => {
         <Header />
 
         <Switch>
-          <Route exact strict path="/" component={HomePage} />
+          <Route exact strict path='/' component={HomePage} />
           <Route
             exact
             strict
-            path="/buy-properties"
+            path='/buy-properties'
             component={SearchPropertyPage}
           />
           <Route
             exact
             strict
-            path="/rental-listings"
+            path='/rental-listings'
             component={SearchPropertyPage}
           />
           <Route
             exact
             strict
-            path="/mortage-calculator"
+            path='/mortage-calculator'
             component={MortageCalculatorPage}
           />
-          <Route exact strict path="/about" component={AboutPage} />
-          <Route exact strict path="/contact" component={ContactPage} />
+          <Route exact strict path='/about' component={AboutPage} />
+          <Route exact strict path='/contact' component={ContactPage} />
           <Route
             exact
             strict
-            path="/buy-properties/:house"
+            path='/buy-properties/:house'
             component={PropertyPage}
           />
           <Route
             exact
             strict
-            path="/rental-listings/:house"
+            path='/rental-listings/:house'
             component={PropertyPage}
           />
-          <Route exact strict path="/my-account" component={MyAccountPage} />
+          <Route exact strict path='/my-account' component={MyAccountPage} />
 
           <Route component={NotFoundPage} />
         </Switch>
@@ -91,7 +91,7 @@ const App = () => {
 
         <ResetScroll />
 
-        <IntroLoader />
+        {/* <IntroLoader /> */}
 
         <Footer />
       </Suspense>
