8ba2fdd1a77f8c57235b964dbe8ef61b4ec4dcde
Grecu <grecdev1@gmail.com>
2020-07-16 16:25:48 +0300

    when property array is empty still show the individual property if the link is accessed

--- src/assets/js/components/search_property/SearchListings.js
+++ src/assets/js/components/search_property/SearchListings.js
@@ -1,11 +1,4 @@
-import React, {
-
-	useContext, 
-	useEffect, 
-	useState,
-	memo
-
-} from 'react';
+import React, { useContext, useEffect, useState, memo } from 'react';
 
 import { GlobalContext } from '@context/GlobalContext';
 import { FetchContext } from '@context/FetchContext';
@@ -17,69 +10,58 @@ import FilterProperties from './FilterProperties';
 import Row from 'react-bootstrap/row';
 
 const SearchListings = () => {
+  const { location } = useContext(GlobalContext);
 
-	const {
+  const {
+    buy_properties,
+    rent_properties,
+    filtered_buy_properties,
+    filtered_rent_properties,
+  } = useContext(FetchContext);
 
-		location
+  let [arr, setArr] = useState([]);
 
-	} = useContext(GlobalContext);
+  useEffect(() => {
+    if (location.includes('buy')) {
+      if (filtered_buy_properties.length > 0) setArr(filtered_buy_properties);
+      else setArr(buy_properties);
+    }
 
-	const {
+    if (location.includes('rent')) {
+      if (filtered_rent_properties.length > 0) setArr(filtered_rent_properties);
+      else setArr(rent_properties);
+    }
+  }, [
+    buy_properties,
+    filtered_buy_properties,
+    rent_properties,
+    filtered_rent_properties,
+  ]);
 
-		buy_properties,
-		rent_properties,
-		filtered_buy_properties,
-		filtered_rent_properties
+  return (
+    <Row
+      id='search-listings'
+      className='p-3 m-0 d-flex flex-column justify-content-start align-items-around'
+    >
+      {arr.length === 0 ? (
+        <div className='search-not-found text-center'>
+          <p className='h1'>No property found, please try again.</p>
 
-	} = useContext(FetchContext);
+          <div className='mx-auto'>
+            <Image src='property-not-found.png' />
+          </div>
+        </div>
+      ) : (
+        <>
+          <FilterProperties array={arr} />
 
-	let [arr, setArr] = useState([]);
-
-	useEffect(() => {
-
-		if(location.includes('buy')) {
-
-			if(filtered_buy_properties.length > 0) setArr(filtered_buy_properties);
-			else setArr(buy_properties);
-		}
-
-		if(location.includes('rent')) {
-
-			if(filtered_rent_properties.length > 0) setArr(filtered_rent_properties);
-			else setArr(rent_properties);
-		}
-
-	}, [buy_properties, filtered_buy_properties, rent_properties, filtered_rent_properties]);
-	
-	return (
-		<Row id='search-listings' className='p-3 m-0 d-flex flex-column justify-content-start align-items-around'>
-
-			{
-				arr.length === 0 ? (
-
-					<div className="search-not-found text-center">
-						<p className='h1'>No property found, please try again.</p>
-						
-						<div className="mx-auto">
-							<Image src='property-not-found.png' />
-						</div>
-					</div>
-
-				) : (
-					<>
-						
-						<FilterProperties array={arr} />
-
-						<div className="w-100 overflow-auto d-flex flex-wrap flex-row justify-content-around align-items-start">
-							<PropertyBox array={arr} />
-						</div>
-
-					</>
-				)
-			}
-
-		</Row>
-	)
-}
+          <div className='w-100 overflow-auto d-flex flex-wrap flex-row justify-content-around align-items-start'>
+            <PropertyBox array={arr} />
+          </div>
+        </>
+      )}
+    </Row>
+  );
+};
 
 export default memo(SearchListings);
